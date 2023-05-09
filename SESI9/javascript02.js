6e3d9960c90ca2758fbc5ff3cef4ad1aaaec92e7
Purnama Indrayuni_2201010578 <115921079+Yuni31@users.noreply.github.com>
2023-05-09 17:04:37 +0800

    UPLOAD

---
+++ SESI9/javascript02.js
@@ -0,0 +1,2 @@
+document.getElementById("teks1").style.color ="plum";
+document.getElementById("teks2").style.color ="red";
---
+++ SESI9/javascript02.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan 2</title>
+</head>
+<body>
+
+    <div id="teks1">
+        <p>Jadilah anak baik tanpa harus dilirik menjadi seorang yang baik</p>
+    </div>
+    <div id="teks2">
+        <p> Jika Pernah bermimpi maka kejarlah mimpimu itu hingga orang lain tidak meremehkanmu kembali lagi</p>
+    </div>
+
+    <script>
+        //Bisa Dipindahkan Ke js atau bisa ttp didalam script
+        //document.getElementById("teks1").style.color ="plum";
+        //document.getElementById("teks2").style.color ="red";
+    </script>
+    <script src="javascript02.js"></script>
+</body>
+</html>
